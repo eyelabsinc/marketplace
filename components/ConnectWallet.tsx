ccaa640fe94aad30128cbfb8dc378de3b42dbc8e
Talha99 <talha840@outlook.com>
2022-10-27 00:05:17 +0300

    Changes

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -83,13 +83,13 @@ const ConnectWallet: FC = () => {
             </DropdownMenu.Item>
           </Link>
 
-          <Link href={``}>
+          {/* <Link href={`./`}>
             
               <a className="group flex w-full cursor-pointer items-center justify-between rounded px-4 py-3 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
                 Buy
               </a>
             
-          </Link>
+          </Link> */}
 
           <Link href={``}>
             
